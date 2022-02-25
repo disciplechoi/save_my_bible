58e6946f1c6ffa0c0e4b325b7b0dccc28ea08a84
disciplechoi <poopoocupcake1991@gmail.com>
2022-02-25 10:40:20 +0900

    modify css

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,14 @@ function App() {
   const [bookId, setBookId] = React.useState('GEN');
   const [bookName, setBookName] = React.useState('Genesis');
   
-  const [chapter, setChapter] = React.useState('');
+  const [chapter, setChapter] = React.useState('1');
   const [chapterList, setChapterList] = React.useState([]);
-  const [verse, setVerse] = React.useState("");
+  const [verse, setVerse] = React.useState('1');
   const [loading, setLoading] = React.useState(false);
+
+  //when click what you wante dto save
+  const [savedVerse, setSavedVerse] = useState("");
+  const [savedVerseList, setSavedVerseList] = useState<string[]>([]);
 
 
   async function getBibleVerse(book = 'Genesis', chapter ='1'){
@@ -75,30 +79,60 @@ function App() {
     
   }
 
+  const saveBibleVerse =(event: React.MouseEvent<HTMLElement>) :void=>{
+    let savedBibleVerse = event.target as HTMLElement;
+    let savedBibleVerseInnerText =  savedBibleVerse.innerText
+    console.log(savedBibleVerse.innerText);
+    setSavedVerse(savedBibleVerseInnerText);
+    setSavedVerseList((currentArray) =>[savedVerse,...currentArray]);
+
+  }
+
   useEffect(()=>{ getBibleBookList();getBibleChapter();getBibleVerse(); },[]);
   
   return (
-    <div className="container container__bible">
-      <h1 className="box-decoration-slice">My Bible</h1>
-  
-      <select onChange={onChangeBibleBookList} value={bookId}>
-        { bibleBookList&&bibleBookList.map(({name, id}: any)=><option value={id}>{name}</option>)}
-      </select>
-      
-      <br/>
-      <select onChange={onChangeBibleChapterList} value={chapter}>
-        { chapterList&&chapterList.map(({number}: any)=><option>{number}</option>)}
-      </select>
+    <div >
+      {/* <h1 className="box-decoration-slice">My Bible</h1> */}
 
-      <div className="container mx-auto">
-        {bibleVerse? bibleVerse.toString().split('[').map((content,index) => 
-        <div className="hover:bg-white">
-        <span key={index}>{index}</span>
-        <p key={content} className="verse">{content} </p>
-        </div>) : "Loading"}
+      <div className="mx-auto" id="container">
+          
+          <input id="tab-1" type="radio" name="tab-group" checked />
+            <label className="lal__content1" htmlFor="tab-1" >Description</label>
+
+            <input  id="tab-2" type="radio" name="tab-group" />
+	          <label htmlFor="tab-2" className="lal__content2">Tab 2</label>
+            
+             <div id="content">
+                  <div id="content-1" className="border-solid border-2 border-white-600">                 
+                    <select onChange={onChangeBibleBookList} value={bookId}>
+                      { bibleBookList&&bibleBookList.map(({name, id}: any)=><option value={id}>{name}</option>)}
+                    </select>
+                    
+                    <br/>
+                    <select onChange={onChangeBibleChapterList} value={chapter}>
+                      { chapterList&&chapterList.map(({number}: any)=><option>{number}</option>)}
+                    </select>
+
+                    <div className="content-1__bibleVerse">
+                      {bibleVerse? bibleVerse.toString().split('[').map((content,index) => 
+                        <p key={chapter+index} className="verse" onClick={saveBibleVerse}>{content} </p>) : "Loading"}
+                    </div>
+                 
+                </div>
+
+                <div id="content-2" className="border-solid border-2 border-white-600 scroll-smooth">
+                   <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
+                </div>      
+             </div>
+          
+            
       </div>
+  
       
-      <Keep book={bookName} chapter={chapter} verse={verse}/>
+      {/* <div className="container--right">
+        <h1>here</h1>
+      {savedVerseList.map(text=><p>{text}</p>)}
+      </div> */}
     </div>
   );
 }
--- src/Keep.tsx
+++ src/Keep.tsx
@@ -1,20 +1,46 @@
-interface KeepProps{
-    book: string;
-    chapter: string;
-    verse: string;
+import { useEffect, useState } from 'react';
+
+
+type KeepProps ={
+    book: string
+    chapter: string
+    verse: string
 }
 
 const defaultProps = {
-    book: "",
-    chapter: "",
-    verse: ""
+    book: "Genesis",
+    chapter: "1",
+    verse: "1"
   };
 
-function Keep({book, chapter,verse} :KeepProps){
+ 
+
+function Keep({book, chapter, verse} :KeepProps = defaultProps){
+    const [savedVerse, setSavedVerse] = useState("");
+    const [savedVerseList, setSavedVerseList] = useState<string[]>([]);
+
+    console.log("chapter: " +chapter+" verse : "+verse);
+    async function getBibleVerse(){
+
+       
+        const response = await fetch(`https://api.esv.org/v3/passage/text/?q=${book}+${chapter}:${verse}&include-passage-references=false&include-verse-numbers=true&include-headings=false&include-passage-horizontal-lines=true&include-first-verse-numbers=true&include-footnotes=false&include-heading-horizontal-lines=true`,
+          {method: 'GET', headers:{'Authorization': 'Token d59a5361e2ab87063ed857904f5f6ce3d1869392'}}
+        );
+    
+        const json = await response.json();
+        setSavedVerse(json.passages);
+        setSavedVerseList((currentArray) =>[savedVerse,...currentArray]);
+        console.log(savedVerseList);
+        
+      }
+    
+      useEffect(()=>{getBibleVerse()},[]);
+
+      
     return(
     <div className="container container__keep">
         <h1>Here are what you saved</h1>
-
+        {savedVerseList.map(text=><p>{text}</p>)}
     
     </div>);
 }
